28ba0d4746d6073fb73e7e86c1409b5a8b089b2b
tayabuz <taya.buzhinskaya@gmail.com>
2017-09-28 21:22:14 +0500

    Добавьте файлы проекта.

---
+++ App1/Game.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+
+    class Game
+    {
+        public Game()
+        {
+            randomNumber = getRandomNumber();
+        }
+        public const int NUMBER_OF_ITEMS = 4;//число позиции в случайном числе
+        public List<int> randomNumber;//коллекция, где хранится случайное число 
+        public int bullsCount = 0; //счетчик быков
+        public int cowsCount = 0; //счетчик коров
+        //public static List<int> userAnswer = new List<int>();//коллекция, где хранится число, которое ввел игрок
+
+        public static List<int> getRandomNumber()//генерирует случайное число
+        {
+            Random rand = new Random();
+            var randomNumber = new List<int>();
+            for (int i = 0; i < NUMBER_OF_ITEMS; i++)
+            {
+                bool isDublicateFound = false;
+                while (isDublicateFound == false)
+                {
+                    var tempRandomNumber = rand.Next(1, 6);
+                    randomNumber.Contains(tempRandomNumber);
+                    if (randomNumber.Contains(tempRandomNumber) == false)
+                    {
+                        randomNumber.Add(tempRandomNumber);
+                        isDublicateFound = true;
+                    }
+                }
+            }
+            return randomNumber;
+        }
+
+        public List<int> getUserAnswer(string userAnswerString)//принимает ответ игрока и преобразует его в числовой массив
+        {
+            var userAnswer = new List<int>();
+            bool userAnswerInvalid = true;
+            int userAnswerOneNumber;
+            while (userAnswerInvalid == true)
+            {
+                try
+                {
+                    if (userAnswerString.Length == NUMBER_OF_ITEMS)
+                    {
+
+                        for (int j = 0; j < userAnswerString.Length; j++)
+                        {
+                            userAnswerOneNumber = (int)Char.GetNumericValue(userAnswerString[j]);
+                            if ((userAnswerOneNumber < 7) && (userAnswerOneNumber > 0))
+                            {
+                                userAnswer.Add(userAnswerOneNumber);
+                                userAnswerInvalid = false;
+                                continue;
+                            }
+
+                        }
+                    }
+                    else
+                    { }
+
+                }
+
+                catch (FormatException)
+                {
+
+                }
+
+            }
+            return userAnswer;
+        }
+
+        public bool checkUserAnswer(List<int> userAnswer)//проверяет ответ игрока
+        {
+            bullsCount = 0;
+            cowsCount = 0;
+            for (int i = 0; i < NUMBER_OF_ITEMS; i++)
+            {
+                if (userAnswer.Contains(randomNumber[i]))
+                {
+                    if (userAnswer[i] == randomNumber[i]) { bullsCount++; }
+                    else { cowsCount++; }
+                }
+            }
+
+            if (bullsCount == 4)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
+
---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Documents;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Game game;
+        public MainPage()
+        {
+            InitializeComponent();
+            game = new Game();
+            
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var tempString = outputBox.Text;
+            if (checkOutput(tempString) == true)
+            {
+                var userAnswerOutput = game.getUserAnswer(outputBox.Text);
+                bool isUserAnswerValid = game.checkUserAnswer(userAnswerOutput);
+
+                if (isUserAnswerValid)
+                {
+                    informationViewer.Text += "\r\n" + "You win";
+                }
+                else
+                {
+                    informationViewer.Text += "\r\n" + outputBox.Text;
+                    informationViewer.Text += "\r\n" + "The quantity of bulls is: " + game.bullsCount + "\r\n";
+                    informationViewer.Text += "The quantity of cows is: " + game.cowsCount + "\r\n";
+                }
+                
+            }
+            outputBox.Text = "";
+
+
+        }
+
+        private void outputBox_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                Button_Click(sender, new RoutedEventArgs());
+                e.Handled = true;
+            }
+            
+        }
+
+        private bool checkOutput(string userAnswerString)
+        {
+            bool userAnswerValid = true;
+            int userAnswerOneNumber;
+            try
+            {
+                if (userAnswerString.Length != Game.NUMBER_OF_ITEMS)
+                {
+                    userAnswerValid = false;
+                }
+                else
+                {
+                    for (int j = 0; j < userAnswerString.Length; j++)
+                    {
+                        userAnswerOneNumber = (int)Char.GetNumericValue(userAnswerString[j]);
+                        if ((userAnswerOneNumber > 6) || (userAnswerOneNumber < 1))
+                        {
+                            userAnswerValid = false;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            catch (FormatException)
+            {
+                userAnswerValid = false;
+            }
+            return userAnswerValid;
+        }
+
+    }
+}
